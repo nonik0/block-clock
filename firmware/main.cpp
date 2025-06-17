dd84a8c2413900764f2fb3154231541f426b58c3
Nick Brown <nick@altonimb.us>
2025-06-17 12:29:18 -0700

    updating 3d files and watchdog fix

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -431,7 +431,8 @@ void setup()
                     Serial.printf("\nEnd");
                     displayEnabled = displayStateOta; });
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
-                        { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); });
+                        { watchdog_update(); // kick the dog
+                          Serial.printf("Progress: %u%%\r", (progress / (total / 100))); });
   ArduinoOTA.onError([](ota_error_t error)
                      {
                 Serial.printf("Error[%u]: ", error);
